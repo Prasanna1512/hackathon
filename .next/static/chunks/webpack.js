f7dfc57ca049f24dbc77628c435a6230af259508
prasannaSR1512 <prasanna.s@kattrapin.com>
2026-04-29 11:38:15 +0530

    Update webpack hot update files and hashes for improved build consistency

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f14c5e5adb861fc"; }
+/******/ 		__webpack_require__.h = function() { return "3b6fb9d90463fa3e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
